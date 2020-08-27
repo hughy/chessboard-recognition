50429ef65c8c8b761fcc5aedb95a4e04bcf81bdc
Hugh Cunningham <hugh.e.cunningham@gmail.com>
2020-08-26 22:10:01 -0700

    Finish implementation of functions to find grid lines
    - Use mean value of filtered image to identify rows and columns in tensor that
      represent edges in chessboard grid
    - Threshold on 80% of max pixel value. This may not work if the light grid
      squares are too dark
    - Trim outer board edge lines from start and end of indices. This may break if
      there are extra lines outside the boundaries of the board
    - Implement function to crop image to board boundaries based on chessboard grid lines

--- ochessr/board.py
+++ ochessr/board.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from typing import Tuple
+
 from PIL import Image
 import numpy as np
 import tensorflow as tf
@@ -7,38 +10,20 @@ def get_board_filters() -> tf.Tensor:
     """Creates a 4-D tensor of filters to identify edges within a chessboard.
     """
     # Create filters
-    h_light_dark = np.array([
-        [1, 1, 1],
-        [0, 0, 0],
-        [-1, -1, -1],
-    ])
-    h_dark_light = np.array([
-        [-1, -1, -1],
-        [0, 0, 0],
-        [1, 1, 1],
-    ])
-    v_light_dark = np.array([
-        [1, 0, -1],
-        [1, 0, -1],
-        [1, 0, -1],
-    ])
-    v_dark_light = np.array([
-        [-1, 0, 1],
-        [-1, 0, 1],
-        [-1, 0, 1],
-    ])
+    h_light_dark = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1],])
+    h_dark_light = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1],])
+    v_light_dark = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1],])
+    v_dark_light = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1],])
     # Stack filters
-    kernel = np.stack(
-        [h_light_dark, h_dark_light, v_light_dark, v_dark_light],
-        axis=-1
-    )
+    kernel = np.stack([h_light_dark, h_dark_light, v_light_dark, v_dark_light], axis=-1)
     # Convert to 4-D
     kernel = np.reshape(kernel, (3, 3, 1, 4))
     # Convert to tensor
     return tf.constant(kernel, dtype=tf.float32)
 
 
-def preprocess_image(img: Image) -> tf.Tensor:
+def preprocess_image(input_img: Image) -> tf.Tensor:
+    img = deepcopy(input_img)
     # Convert to grayscale
     img = img.convert(mode="L")
     # Convert to array
@@ -48,9 +33,8 @@ def preprocess_image(img: Image) -> tf.Tensor:
     # Convert to tensor
     return tf.constant(img, dtype=tf.float32)
 
-def main():
-    img = Image.open("img/board0.png")
-    img = preprocess_image(img)
+
+def get_grid_indices(img: tf.Tensor) -> Tuple[np.array, np.array]:
     kernel = get_board_filters()
 
     # Apply convolution
@@ -59,14 +43,55 @@ def main():
     filtered_img = tf.clip_by_value(filtered_img, 0, 255)
     # Collapse output channels into a single channel
     filtered_img = tf.math.reduce_max(filtered_img, axis=-1)
+    # Remove first dimension
+    filtered_img = tf.squeeze(filtered_img, axis=0)
 
     # Get indices of vertical and horizontal edges
-    v_mean = tf.math.reduce_mean(filtered_img, axis=1)
-    h_mean = tf.math.reduce_mean(filtered_img, axis=2)
-    # Use 3/4 of maximum pixel value as threshold
+    v_mean = tf.math.reduce_mean(filtered_img, axis=0)
+    h_mean = tf.math.reduce_mean(filtered_img, axis=1)
+    # Use 80% of maximum pixel value as threshold
     # NOTE: this threshold could be a problem if the "light" grid squares are too dark
-    v_indices = tf.where(v_mean > (255 / 1.5))
-    h_indices = tf.where(h_mean > (255 / 1.5))
+    v_indices = tf.where(v_mean > (255 / 1.25))
+    h_indices = tf.where(h_mean > (255 / 1.25))
+    # Trim boundaries
+    # NOTE: this may not work if image contains extra information outside board
+    v_indices = v_indices[1:-1]
+    h_indices = h_indices[1:-1]
+    # Convert to 1-D numpy arrays
+    v_indices = tf.squeeze(v_indices, axis=-1).numpy()
+    h_indices = tf.squeeze(h_indices, axis=-1).numpy()
+
+    # Verify number of grid lines
+    if len(v_indices) != 14 or len(h_indices) != 14:
+        raise ValueError("Detected an unexpected number of chessboard grid lines.")
+
+    return v_indices, h_indices
+
+
+def crop_board_image(
+    board_img: tf.Tensor,
+    grid_square_length: int,
+    vert_indices: np.array,
+    horiz_indices: np.array,
+) -> tf.Tensor:
+    # Crop image to grid squares
+    v_start = vert_indices[0] - grid_square_length + 1
+    v_end = vert_indices[-1] + grid_square_length
+    h_start = horiz_indices[0] - grid_square_length + 1
+    h_end = horiz_indices[-1] + grid_square_length
+    return img[:, h_start:h_end, v_start:v_end, :]
+
+
+def main():
+    input_img = Image.open("img/board0.png")
+
+    img = preprocess_image(input_img)
+
+    # Identify gridline indices
+    vert_indices, horiz_indices = get_grid_indices(img)
+
+    # Determine length of grid square sides
+    grid_square_length = vert_indices[2] - vert_indices[0]
 
 
 if __name__ == "__main__":
